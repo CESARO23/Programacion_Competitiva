9016848e04ea2703be5d609e7cc49759daf6aa5d
CESARO23 <mamanivillenaj@gmail.com>
2020-12-22 21:26:18 -0400

    Intersecting Lines

---
+++ UVAJudge/Geometry/Lines/p378.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ZERO 1e-8
+#define INF 1e100
+
+#define equal(a,b) (fabs((a)-(b))<ZERO)
+
+using namespace std;
+
+struct point
+{
+	double x,y;
+};
+
+int main()
+{
+	point L1[2], L2[2];
+	printf("%.14lf\n", ZERO);
+	return 0;
+}
